ee8abf5cf2ddaf8ade956602612fcda476529cfa
Lucas Auman <lwauman@uncg.edu>
2016-08-26 18:41:51 -0400

    fixing bugs and working on prime

--- hw1/src/lwauman_hwk1.java
+++ hw1/src/lwauman_hwk1.java
@@ -31,12 +31,11 @@ public class lwauman_hwk1{
                 return false;
             Class itemClass = item.getClass();
             if(itemClass.equals(Integer.class))
-                return ((Integer)item).doubleValue()/2 % 1 == 0;
+                return Math.sqrt((Integer)item) % 1 == 0;
             else if(itemClass.equals(Double.class))
                 return Math.sqrt((Double)item) % 1 == 0;
-            else if(itemClass.equals(Float.class)){
+            else if(itemClass.equals(Float.class))
                 return Math.sqrt((Float)item) % 1 == 0;
-            }
             else if(itemClass.equals(Byte.class))
                 return Math.sqrt((Byte)item) % 1 == 0;
             else if(itemClass.equals(Short.class))
@@ -46,7 +45,38 @@ public class lwauman_hwk1{
             else
                 return false;
         }
-        
+    }
+    public static class Prime<T> implements Check<T>{
+        @Override
+        public boolean test(T item) {
+            if(item == null)
+                return false;
+            Class itemClass = item.getClass();
+            if(itemClass.equals(Integer.class)){
+                int num = (Integer)item;
+                if(num<2)
+                    return false;
+                else{
+                    for(int i=2; i<=num/2; i++){
+                    if(num % i == 0)
+                        return false;
+                    }
+                    return true;
+                }
+            }    
+            else if(itemClass.equals(Double.class))
+                return Math.sqrt((Float)item) % 1 == 0;
+            else if(itemClass.equals(Float.class))
+                return Math.sqrt((Float)item) % 1 == 0;
+            else if(itemClass.equals(Byte.class))
+                return Math.sqrt((Byte)item) % 1 == 0;
+            else if(itemClass.equals(Short.class))
+                return Math.sqrt((Short)item) % 1 == 0;
+            else if(itemClass.equals(Long.class))
+                return Math.sqrt((Long)item) % 1 == 0;
+            else
+                return false;
+        }  
     }
     public static <T> boolean contains(T[] input, Check<T> function){
         for(T item : input)
@@ -55,18 +85,18 @@ public class lwauman_hwk1{
         return false;
     }
     public static void main(String[] args) {
-        Integer[] iarr = {100, null, 37, 49, -6};
-        Integer[] iarr2 = {23, 37, 17};
-        Double[] iarr3 = {100.0, null, 37.1, 49.2, -1.3};
-        Double[] iarr4 = {23.0, 37.2, 17.3};
-        Long[] iarr5 = {100L, null, 37L, 49L, -6L};
-        Long[] iarr6 = {23L, 37L, 17L};
-        Byte[] iarr7 = {100, null, 37, 49, -6};
-        Byte[] iarr8 = {23, 37, 17};
-        Short[] iarr9 = {100, null, 37, 49, -6};
-        Short[] iarr10 = {23, 37, 17};
-        Float[] iarr11 = {100.0f, null, 37.1f, 49.2f, -1.3f};
-        Float[] iarr12 = {23.0f, 37.2f, 49.1f};
+        Integer[] iarr = {null, -1, 49, 3};
+        Integer[] iarr2 = {0, 6, 8};
+        Double[] iarr3 = {null, -1.0, 49.0, 3.0};
+        Double[] iarr4 = {0.0, 6.1, 8.2};
+        Long[] iarr5 = {null, -1L, 49L, 3L};
+        Long[] iarr6 = {0L, 6L, 8L};
+        Byte[] iarr7 = {null, -1, 49, 3};
+        Byte[] iarr8 = {0, 6, 8};
+        Short[] iarr9 = {null, -1, 49, 3};
+        Short[] iarr10 = {0, 6, 8};
+        Float[] iarr11 = {null, -1.0f, 49.0f, 3.0f};
+        Float[] iarr12 = {0.0f, 6.1f, 8.2f};
         String[] iarr13 = {"apple"};
         System.out.println("        Contains Negative Tests");
         System.out.println("Integer Array (true expected).  Result: "+contains(iarr, new Negative()));
@@ -83,7 +113,7 @@ public class lwauman_hwk1{
         System.out.println("Float Array (false expected).   Result: "+contains(iarr12, new Negative()));
         System.out.println("String Array (false expected).  Result: "+contains(iarr13, new Negative()));
         System.out.println("");
-        System.out.println("        Contains PerfectSquare Tests");
+        System.out.println("        Contains Perfect Square Tests");
         System.out.println("Integer Array (true expected).  Result: "+contains(iarr, new PerfectSquare()));
         System.out.println("Integer Array (false expected). Result: "+contains(iarr2, new PerfectSquare()));
         System.out.println("Double Array (true expected).   Result: "+contains(iarr3, new PerfectSquare()));
@@ -97,11 +127,10 @@ public class lwauman_hwk1{
         System.out.println("Float Array (true expected).    Result: "+contains(iarr11, new PerfectSquare()));
         System.out.println("Float Array (false expected).   Result: "+contains(iarr12, new PerfectSquare()));
         System.out.println("String Array (false expected).  Result: "+contains(iarr13, new PerfectSquare()));
-        /*
         System.out.println("");
         System.out.println("        Contains Prime Tests");
-        System.out.println("Integer Array (true expected).  Result: "+contains(iarr, new PerfectSquare()));
-        System.out.println("Integer Array (false expected). Result: "+contains(iarr2, new PerfectSquare()));
+        System.out.println("Integer Array (true expected).  Result: "+contains(iarr, new Prime()));
+        System.out.println("Integer Array (false expected). Result: "+contains(iarr2, new Prime()));
         System.out.println("Double Array (true expected).   Result: "+contains(iarr3, new PerfectSquare()));
         System.out.println("Double Array (false expected).  Result: "+contains(iarr4, new PerfectSquare()));
         System.out.println("Long Array (true expected).     Result: "+contains(iarr5, new PerfectSquare()));
@@ -114,7 +143,6 @@ public class lwauman_hwk1{
         System.out.println("Float Array (false expected).   Result: "+contains(iarr12, new PerfectSquare()));
         System.out.println("String Array (false expected).  Result: "+contains(iarr13, new PerfectSquare()));
         System.out.println("");
-        */
     }
 }
 
